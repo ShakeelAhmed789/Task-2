6c5b22d94d61e199adf7cf46b84d075051fdf56a
Shakeel <ahshakeel1994@gmail.com>
2026-05-11 20:52:39 +0500

    Add project files.

---
+++ Task-2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Task_2_demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(1, 50);
+            bool sign = true;
+            int guessedNumber;
+            int counter = 0;
+
+            //Console.WriteLine(secretNumber);
+
+            Console.WriteLine("\n****** Number Guessing Game ******\n");
+            Console.WriteLine("Rules:\n" +
+                "If the input number is 10 numbers higher than the secret number, the program flags \"Too high!\"\n" +
+                "If the input number is 10 numbers lower than the secret number, the program flags \"Too low!\"\n");
+
+            Console.WriteLine("Guess the secret number.");
+            Console.WriteLine("Hint: The number is between 0 and 50.");
+
+            while (sign)
+            {
+
+                Console.Write("\nPlease enter a number: ");
+
+                while (!int.TryParse(Console.ReadLine(), out guessedNumber))
+                {
+                    Console.Write("Invalid input! Try again.\n\nPlease enter a number: ");
+                }
+
+                counter++;
+
+                if (guessedNumber == secretNumber)
+                    sign = false;
+                if (guessedNumber > secretNumber + 10)
+                    Console.WriteLine("Too high! Try again.");
+                else if (guessedNumber < secretNumber - 10)
+                    Console.WriteLine("Too low! Try again.");
+
+            }
+
+            Console.WriteLine("\nCongratulations! You found the secret number!");
+
+            Console.WriteLine($"The secret number was: {secretNumber}");
+
+            Console.WriteLine($"You guessed the number in {counter} attempts.");
+        }
+    }
+}
+
+/*
+
+    Research Question:
+    In your last task, you looked at int.Parse(). 
+    Now, explain: What is the out keyword used for in int.TryParse(input, out result)? 
+    (Why can't we just say int result = int.TryParse(input);?)
+
+    The out keyword is used to return an extra value from a method.
+
+    In int.TryParse(input, out result):
+
+    TryParse() returns true or false
+    out result stores the converted integer value
+
+    Example:
+    bool success = int.TryParse("25", out int result);
+    success → true
+    result → 25
+
+    We cannot write:
+    int result = int.TryParse(input);
+
+    because TryParse() returns a bool, not an int. The integer value is returned using the out parameter.
+ 
+*/
